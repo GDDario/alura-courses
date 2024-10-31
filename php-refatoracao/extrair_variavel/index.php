07f36835a12c25c218b3cb2acbe1fcb764bd123e
GDDario <gd.dario.dev@gmail.com>
2024-10-31 10:28:53 -0300

    php-refatoracao: extrair_variavel

--- php-refatoracao/extrair_variavel/index.php
+++ php-refatoracao/extrair_variavel/index.php
@@ -2,7 +2,15 @@
 
 function verificaSeEmailEhValido(): string
 {
-    return filter_var($_GET['email'], FILTER_VALIDATE_EMAIL) ? "O seu e-mail é: " . $_GET['email'] : "O seu e-mail não é válido";
+    $email = $_GET['email'];
+    $mensagemSucesso = "O seu e-mail é: " . $_GET['email'];
+    $mensagemErro = "O seu e-mail não é válido";
+
+    if (filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) {
+        return $mensagemSucesso;
+    }
+
+    return $mensagemErro;
 }
 
 echo verificaSeEmailEhValido();
